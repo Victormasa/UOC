a86d0b88f80989a5ee8c961034a8f2fb566dea04
victor martinez santos <victor.martinez@irbbarcelona.org>
2025-05-05 12:11:09 +0200

    PR2

--- 2024_2/fonaments de programacio/test.c
+++ 2024_2/fonaments de programacio/test.c
@@ -1,26 +1,31 @@
-#include <stdbool.h>
 #include <stdio.h>
+#define NUMBER 10
 
-int main() {
-    char ch;
-    bool bo;
-    int in1, in2;
-    float re;
-    FILE* fileToWrite;
+void fill(int table[]) { 
+    int i;
 
-    fileToWrite = fopen("file_name.txt", "w");
-    ch = 'a';
-    bo = true;
-    in1 = 2;
-    in2 = 3;
-    re = 2.0;
+    *table = 5;
 
-    fprintf(fileToWrite, "%c \n", ch);
-    fprintf(fileToWrite, "%d \n", bo);
-    fprintf(fileToWrite, "%d \t %d \t %d \n", in1, in2, in1 + in2);
-    fprintf(fileToWrite, "%f \n", re);
+    for (i = 0; i< NUMBER; i++){
+        table[i] ++;
+    }
+} 
 
-    fclose(fileToWrite);
+
+int main() { 
+    /* Variable definition */ 
+    int i;
+    int data[NUMBER];
+
+    for (i = 0; i < NUMBER; i++){
+        data[i] = 3;
+    }
+
+    fill(data);
+
+    for (i =0 ; i < NUMBER; i++){
+        printf("%d ", data[i]);
+    }
 
     return 0;
 }
